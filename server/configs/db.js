8151d82aa0ad9bcb2a569e2c4e3614126aad8a2d
Madhusmita <mjena2818@gmail.com>
2025-06-03 21:22:51 +0530

    Setup backend structure for user, hotel, and room features

--- server/configs/db.js
+++ server/configs/db.js
@@ -4,10 +4,7 @@ const connectDB = async () => {
     try{
         // mongoose.connection.on('connected', ()=> console.log("Database connected"));
         // await mongoose.connect(`${process.env.MONGODB_URI}/Hotel-Booking-DB`)
-        const conn = await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        const conn = await mongoose.connect(process.env.MONGODB_URI);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     }catch(error){
         console.log(error.message);        
---
+++ server/controller/userController.js
@@ -0,0 +1,33 @@
+//GET /api/user/
+
+export const getUserData =async(req, res) => {
+    try{
+        const role = req.user.role;
+        const recentSearchedCities = req.user.recentSearchedCities;
+        res.json({success: true, message: recentSearchedCities})
+    }catch(error){
+        res.json({success: false, message: error.message})        
+        
+    }
+}
+
+// Store User's Recently Searched Cities
+export const storeRecentSearchedCities = async (req, res) => {
+  try {
+    const { recentSearchedCity } = req.body;
+    const user = await req.user;
+
+    if (user.recentSearchedCities.length < 3) {
+      user.recentSearchedCities.push(recentSearchedCity);
+    } else {
+      user.recentSearchedCities.shift(); // Remove the oldest entry
+      user.recentSearchedCities.push(recentSearchedCity);
+    }
+
+    await user.save(); //save updated user data in mongoDB
+
+    res.json({ success: true, message: "City added" });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
